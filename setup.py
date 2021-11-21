52712b5e21c296ec46d4990e0b3e1958974ed0ef
Neradoc <neraOnGit@ri1.fr>
2021-11-21 18:51:13 +0100

    black unhappy

--- setup.py
+++ setup.py
@@ -57,7 +57,6 @@ setup(
     ],
     # What does your project relate to?
     keywords="adafruit blinka circuitpython micropython consumer_control_extended HID",
-
     # You can just specify the packages manually here if your project is
     # simple. Or you can use find_packages().
     # TODO: IF LIBRARY FILES ARE A PACKAGE FOLDER,
